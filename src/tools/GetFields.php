ec2b236deaee627190411edd45ef6df4e894e289
Mark Huot <mark@markhuot.com>
2025-10-10 14:55:33 -0400

    wip

--- src/tools/GetFields.php
+++ src/tools/GetFields.php
@@ -18,7 +18,7 @@ class GetFields
         description: <<<'END'
         Get a list of all fields in Craft CMS. This is useful for understanding the available fields, their
         configurations, and the field handle that must be used when creating or updating entries.
-        
+
         You can pass an optional fieldLayoutId, if you know it, to only get the fields associated with that layout.
         END
     )]
